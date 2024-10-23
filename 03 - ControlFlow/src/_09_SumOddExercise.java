303f155b034157214090b32e1ea8c35e7b7d4cb8
Gildo Neto <32935771+gildoneto@users.noreply.github.com>
2024-10-23 15:52:18 -0300

    section 6 - sum odd exercise

---
+++ 03 - ControlFlow/src/_09_SumOddExercise.java
@@ -0,0 +1,34 @@
+public class _09_SumOddExercise {
+    public static void main(String[] args) {
+        System.out.println(sumOdd(1, 100));
+    }
+
+    public static boolean isOdd(int number){
+        if (number > 0){
+            if(number % 2 == 0){
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public static int sumOdd(int start, int end){
+
+        if(start > end || start <= 0){
+            return -1;
+        }
+        int sum = 0;
+
+
+        for(int i = start; i <= end; i++){
+            if(isOdd(i)){
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
+}
