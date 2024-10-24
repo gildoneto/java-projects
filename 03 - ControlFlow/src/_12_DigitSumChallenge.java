0f4304697f6b633ae7372bfafcd1608df1d89198
Gildo Neto <32935771+gildoneto@users.noreply.github.com>
2024-10-24 10:12:14 -0300

    section 6 - digit sum challenge

---
+++ 03 - ControlFlow/src/_12_DigitSumChallenge.java
@@ -0,0 +1,27 @@
+public class _12_DigitSumChallenge {
+    public static void main(String[] args) {
+
+        System.out.println("The sum of 1234 is " + sumDigits(1234));
+        System.out.println("The sum of -125 is " + sumDigits(-125));
+        System.out.println("The sum of 4 is " + sumDigits(4));
+        System.out.println("The sum of 32123 is " + sumDigits(32123));
+    }
+
+    public static int sumDigits (int number) {
+        if(number < 0) {
+            return -1;
+        }
+
+        int sum = 0;
+
+        while (number > 9) {
+            sum += number % 10;
+            number = number / 10;
+        }
+
+        sum += number;
+        return sum;
+    }
+
+
+}
